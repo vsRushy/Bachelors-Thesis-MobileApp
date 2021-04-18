a096384776ba906720e95f022cd4723dd15ba6a0
vsRushy <vsRushy@users.noreply.github.com>
2021-04-18 20:33:55 +0200

    Changed app title. Added Firebase to the project.

--- src/ai_mobile_app/README.md
+++ src/ai_mobile_app/README.md
@@ -1,4 +1,4 @@
-# mobile_app
+# ai_mobile_app
 
 A new Flutter project.
 
--- src/ai_mobile_app/android/app/src/main/kotlin/com/tfg/ai_mobile_app/MainActivity.kt
+++ src/ai_mobile_app/android/app/src/main/kotlin/com/tfg/ai_mobile_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobile_app
+package com.tfg.ai_mobile_app
 
 import io.flutter.embedding.android.FlutterActivity
 
