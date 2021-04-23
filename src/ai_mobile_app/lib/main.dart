cf1ad7060f93066eeddafa4b4092fecb29f2ca29
vsRushy <vsRushy@users.noreply.github.com>
2021-04-24 00:17:06 +0200

    Added Provider package.

--- src/ai_mobile_app/lib/main.dart
+++ src/ai_mobile_app/lib/main.dart
@@ -1,19 +1,26 @@
+import 'package:ai_mobile_app/models/custom_user.dart';
 import 'package:ai_mobile_app/screens/wrapper.dart';
+import 'package:ai_mobile_app/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<CustomUser?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- src/ai_mobile_app/lib/screens/wrapper.dart
+++ src/ai_mobile_app/lib/screens/wrapper.dart
@@ -1,9 +1,14 @@
+import 'package:ai_mobile_app/models/custom_user.dart';
 import 'package:ai_mobile_app/screens/authentication/authentication.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<CustomUser?>(context);
+    print(user);
+
     return Authentication();
   }
 }
