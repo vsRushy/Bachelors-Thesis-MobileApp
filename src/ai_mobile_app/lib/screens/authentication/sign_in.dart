58865c21d40fc14afe5dd87b041588b7a40093f5
vsRushy <vsRushy@users.noreply.github.com>
2021-05-01 11:45:18 +0200

    Fixed overflow.

--- src/ai_mobile_app/lib/screens/authentication/sign_in.dart
+++ src/ai_mobile_app/lib/screens/authentication/sign_in.dart
@@ -63,136 +63,136 @@ class _SignInState extends State<SignIn> {
                         color: Colors.white,
                       ),
                       Positioned.fill(
-                        top: -60,
+                        top: -90,
                         bottom: 0,
                         child: Align(
                           alignment: Alignment.center,
-                          child: Container(
-                            margin: EdgeInsets.all(30.0),
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.5),
-                                  blurRadius: 10.0,
-                                  spreadRadius: 5.0,
-                                  offset: Offset(0, 3),
+                          child: SingleChildScrollView(
+                            child: Container(
+                              margin: EdgeInsets.all(30.0),
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10.0),
                                 ),
-                              ],
-                            ),
-                            child: Form(
-                              key: _formKey,
-                              child: Column(
-                                children: <Widget>[
-                                  Text(
-                                    'LOGIN',
-                                    style: customTextTitleStyle.copyWith(
-                                      fontSize: 20.0,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 20.0,
-                                  ),
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      'E-MAIL',
-                                      style: customTextTitleStyle.copyWith(
-                                        fontSize: 14.0,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  TextFormField(
-                                    decoration: customInputDecoration.copyWith(
-                                        hintText: 'e-mail'),
-                                    validator: (value) {
-                                      return (value!.isEmpty)
-                                          ? 'Please, enter an e-mail.'
-                                          : null;
-                                    },
-                                    onChanged: (value) {
-                                      setState(() {
-                                        email = value;
-                                      });
-                                    },
-                                  ),
-                                  SizedBox(
-                                    height: 30.0,
-                                  ),
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      'PASSWORD',
-                                      style: customTextTitleStyle.copyWith(
-                                        fontSize: 14.0,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  TextFormField(
-                                    decoration: customInputDecoration.copyWith(
-                                        hintText: 'password'),
-                                    obscureText: true,
-                                    validator: (value) {
-                                      return (value!.length < 6)
-                                          ? 'The password must be at least 6 characters long.'
-                                          : null;
-                                    },
-                                    onChanged: (value) {
-                                      setState(() {
-                                        password = value;
-                                      });
-                                    },
-                                  ),
-                                  SizedBox(
-                                    height: 20.0,
-                                  ),
-                                  ElevatedButton(
-                                    child: Text('Sign in'),
-                                    style: ElevatedButton.styleFrom(
-                                      primary: Colors.blueAccent,
-                                      minimumSize: Size(140, 40),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(30.0),
-                                      ),
-                                    ),
-                                    onPressed: () async {
-                                      if (_formKey.currentState!.validate()) {
-                                        setState(() {
-                                          isLoading = true;
-                                        });
-
-                                        dynamic result = await _auth
-                                            .signInEmail(email, password);
-                                        if (result == null) {
-                                          setState(() {
-                                            error =
-                                                'An account with the credentials provided could not be found.';
-                                            isLoading = false;
-                                          });
-                                        }
-                                      }
-                                    },
-                                  ),
-                                  SizedBox(height: 12.0),
-                                  Text(
-                                    error,
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 11,
-                                    ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.5),
+                                    blurRadius: 10.0,
+                                    spreadRadius: 5.0,
+                                    offset: Offset(0, 3),
                                   ),
                                 ],
+                              ),
+                              child: Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      'LOGIN',
+                                      style: customTextTitleStyle.copyWith(
+                                        fontSize: 20.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        'E-MAIL',
+                                        style: customTextTitleStyle.copyWith(
+                                          fontSize: 14.0,
+                                        ),
+                                      ),
+                                    ),
+                                    /*SizedBox(
+                                      height: 10,
+                                    ),*/
+                                    TextFormField(
+                                      decoration: customInputDecoration
+                                          .copyWith(hintText: 'e-mail'),
+                                      validator: (value) {
+                                        return (value!.isEmpty)
+                                            ? 'Please, enter an e-mail.'
+                                            : null;
+                                      },
+                                      onChanged: (value) {
+                                        setState(() {
+                                          email = value;
+                                        });
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: 30.0,
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        'PASSWORD',
+                                        style: customTextTitleStyle.copyWith(
+                                          fontSize: 14.0,
+                                        ),
+                                      ),
+                                    ),
+                                    /*SizedBox(
+                                      height: 10,
+                                    ),*/
+                                    TextFormField(
+                                      decoration: customInputDecoration
+                                          .copyWith(hintText: 'password'),
+                                      obscureText: true,
+                                      validator: (value) {
+                                        return (value!.length < 6)
+                                            ? 'The password must be at least 6 characters long.'
+                                            : null;
+                                      },
+                                      onChanged: (value) {
+                                        setState(() {
+                                          password = value;
+                                        });
+                                      },
+                                    ),
+                                    
+                                    ElevatedButton(
+                                      child: Text('Sign in'),
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Colors.blueAccent,
+                                        minimumSize: Size(140, 40),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30.0),
+                                        ),
+                                      ),
+                                      onPressed: () async {
+                                        if (_formKey.currentState!.validate()) {
+                                          setState(() {
+                                            isLoading = true;
+                                          });
+
+                                          dynamic result = await _auth
+                                              .signInEmail(email, password);
+                                          if (result == null) {
+                                            setState(() {
+                                              error =
+                                                  'An account with the credentials provided could not be found.';
+                                              isLoading = false;
+                                            });
+                                          }
+                                        }
+                                      },
+                                    ),
+                                    SizedBox(height: 12.0),
+                                    Text(
+                                      error,
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 11,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
