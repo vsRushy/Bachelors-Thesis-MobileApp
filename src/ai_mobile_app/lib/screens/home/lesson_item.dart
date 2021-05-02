26bf121502eb2b6b163ac9e9833ee6887fe663d3
vsRushy <vsRushy@users.noreply.github.com>
2021-05-03 00:41:43 +0200

    Big lesson refactor.

--- src/ai_mobile_app/lib/screens/home/lesson_item.dart
+++ src/ai_mobile_app/lib/screens/home/lesson_item.dart
@@ -2,10 +2,10 @@ import 'package:ai_mobile_app/models/custom_test.dart';
 import 'package:flutter/material.dart';
 import 'package:ai_mobile_app/screens/home/lessons.dart';
 
-class LessonItem extends StatelessWidget {
+/*class LessonItem extends StatelessWidget {
   final CustomTest test;
 
-  LessonItem({required this.test});
+  LessonItem({ required this.test });
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +20,123 @@ class LessonItem extends StatelessWidget {
             radius: 25.0,
             backgroundColor: Colors.lightBlue,
           ),
+        ),
+      ),
+    );
+  }
+}*/
+
+class LessonItem extends StatelessWidget {
+  final IconData? icon;
+  final String? title;
+  final String? subTitle;
+  final String? description;
+
+  LessonItem({
+    required IconData this.icon,
+    required String this.title,
+    required String this.subTitle,
+    required String this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            blurRadius: 10,
+            spreadRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Card(
+        color: Colors.lightBlue,
+        shadowColor: Colors.black,
+        child: Column(
+          children: [
+            ListTile(
+              leading: Icon(
+                icon,
+                color: Colors.white,
+                size: 40,
+              ),
+              title: Text(
+                title!,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                ),
+              ),
+              subtitle: Text(
+                subTitle!,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                description!,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ButtonBar(
+                  alignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      child: Text(
+                        'START',
+                        style: TextStyle(color: Colors.lightBlue),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        minimumSize: Size(140, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                TextButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.info,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'More info',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- src/ai_mobile_app/lib/screens/home/lessons.dart
+++ src/ai_mobile_app/lib/screens/home/lessons.dart
@@ -24,278 +24,47 @@ class _LessonsState extends State<Lessons> {
     return ListView(
       padding: EdgeInsets.all(25),
       children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(1),
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                blurRadius: 10,
-                spreadRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Card(
-            color: Colors.lightBlue,
-            shadowColor: Colors.black,
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(
-                    Icons.looks_one,
-                    color: Colors.white,
-                    size: 40,
-                  ),
-                  title: Text(
-                    'Lesson 1',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 23,
-                    ),
-                  ),
-                  subtitle: Text(
-                    'Environment setup',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'This is the first lesson.',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ButtonBar(
-                      alignment: MainAxisAlignment.start,
-                      children: [
-                        ElevatedButton(
-                          child: Text(
-                            'START',
-                            style: TextStyle(color: Colors.lightBlue),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            minimumSize: Size(140, 40),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                          ),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                    TextButton(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.info,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            'More info',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        LessonItem(
+          icon: Icons.looks_one,
+          title: 'Lesson 1',
+          subTitle: 'Environment setup',
+          description: 'This is the first lesson.',
         ),
         SizedBox(
           height: 25.0,
         ),
-        Container(
-          padding: EdgeInsets.all(1),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                blurRadius: 10,
-                spreadRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Card(
-            color: Colors.lightBlue,
-            shadowColor: Colors.black,
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.looks_two),
-                  title: Text(
-                    'Lesson 2',
-                  ),
-                  subtitle: Text(
-                    'Secondary Text',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'Lorem Ipsum Dolor Avet Sit Amet.',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      child: Text('ACTION 1'),
-                      onPressed: () {},
-                    ),
-                    TextButton(
-                      child: Text('ACTION 2'),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        LessonItem(
+          icon: Icons.looks_two,
+          title: 'Lesson 2',
+          subTitle: 'Simple ANN movement',
+          description: 'This is the second lesson.',
         ),
         SizedBox(
           height: 25.0,
         ),
-        Container(
-          padding: EdgeInsets.all(1),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                blurRadius: 10,
-                spreadRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Card(
-            color: Colors.lightBlue,
-            shadowColor: Colors.black,
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.looks_3),
-                  title: Text(
-                    'Lesson 3',
-                  ),
-                  subtitle: Text(
-                    'Secondary Text',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'Lorem Ipsum Dolor Avet Sit Amet.',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      child: Text('ACTION 1'),
-                      onPressed: () {},
-                    ),
-                    TextButton(
-                      child: Text('ACTION 2'),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        LessonItem(
+          icon: Icons.looks_3,
+          title: 'Lesson 3',
+          subTitle: 'Car ANN movement #1',
+          description: 'This is the third lesson.',
         ),
         SizedBox(
           height: 25.0,
         ),
-        Container(
-          padding: EdgeInsets.all(1),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                blurRadius: 10,
-                spreadRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Card(
-            color: Colors.lightBlue,
-            shadowColor: Colors.black,
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.looks_4),
-                  title: Text(
-                    'Lesson 4',
-                  ),
-                  subtitle: Text(
-                    'Secondary Text',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'Lorem Ipsum Dolor Avet Sit Amet.',
-                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      child: Text('ACTION 1'),
-                      onPressed: () {},
-                    ),
-                    TextButton(
-                      child: Text('ACTION 2'),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        LessonItem(
+          icon: Icons.looks_4,
+          title: 'Lesson 4',
+          subTitle: 'Car ANN movement #2',
+          description: 'This is the fourth lesson.',
+        ),
+        SizedBox(
+          height: 25.0,
+        ),
+        LessonItem(
+          icon: Icons.looks_5,
+          title: 'Lesson 5',
+          subTitle: 'Own Artificial Neural Network',
+          description: 'This is the fifth lesson.',
         ),
       ],
     );
