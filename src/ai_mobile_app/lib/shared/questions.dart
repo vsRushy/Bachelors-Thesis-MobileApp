b435e947a3fd069f08573c42e29b8ba29156c57a
gg1 <gerardmarcosfreixas@gmail.com>
2021-06-08 13:51:31 +0200

    Test completion viewer.

--- src/ai_mobile_app/lib/shared/questions.dart
+++ src/ai_mobile_app/lib/shared/questions.dart
@@ -1,14 +1,23 @@
 import 'package:ai_mobile_app/models/custom_question.dart';
 import 'package:ai_mobile_app/models/question_category.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicators/step_page_indicator.dart';
 
-class Questions extends StatelessWidget {
+class Questions extends StatefulWidget {
   final QuestionCategory? category;
 
   const Questions({
     Key? key,
     required this.category,
   }) : super(key: key);
+
+  @override
+  _QuestionsState createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  final _pageController = PageController();
+  final _currentPageNotifier = ValueNotifier<int>(0);
 
   Widget _buildQuestion({required CustomQuestion question}) {
     return Padding(
@@ -31,13 +40,36 @@ class Questions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      itemCount: category!.questions!.length,
-      itemBuilder: (context, index) {
-        final question = category!.questions![index];
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: widget.category!.questions!.length,
+            itemBuilder: (context, index) {
+              final question = widget.category!.questions![index];
 
-        return _buildQuestion(question: question);
-      },
+              return _buildQuestion(question: question);
+            },
+            onPageChanged: (int index) {
+              _currentPageNotifier.value = index;
+            },
+          ),
+        ),
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(8),
+          child: StepPageIndicator(
+            itemCount: 2,
+            currentPageNotifier: _currentPageNotifier,
+            size: 16,
+            onPageSelected: (int index) {
+              if (_currentPageNotifier.value > index)
+                _pageController.jumpToPage(index);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
