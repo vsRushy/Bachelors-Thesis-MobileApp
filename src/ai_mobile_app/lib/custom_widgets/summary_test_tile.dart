3007de70943262d5da24a281cf758e5175026175
gg1 <gerardmarcosfreixas@gmail.com>
2021-06-12 13:55:12 +0200

    Changed test done tile style.

--- src/ai_mobile_app/lib/custom_widgets/summary_test_tile.dart
+++ src/ai_mobile_app/lib/custom_widgets/summary_test_tile.dart
@@ -12,12 +12,16 @@ class SummaryTestTile extends StatelessWidget {
       padding: EdgeInsets.all(10),
       child: Card(
         margin: EdgeInsets.fromLTRB(20, 8, 20, 8),
-        child: ListTile(
-          title: Text(
-            "Test ${test.testId.toString()}",
-          ),
-          subtitle: Text(
-            "Mark: ${test.mark}   Correct answers: ${test.correctAnswers}   Incorrect answers: ${test.incorrectAnswers}",
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: ListTile(
+            leading: Icon(Icons.ac_unit_outlined),
+            title: Text(
+              "Test ${test.testId.toString()}",
+            ),
+            subtitle: Text(
+              "Mark: ${test.mark}/100.0   Correct answers: ${test.correctAnswers}   Incorrect answers: ${test.incorrectAnswers}",
+            ),
           ),
         ),
       ),
