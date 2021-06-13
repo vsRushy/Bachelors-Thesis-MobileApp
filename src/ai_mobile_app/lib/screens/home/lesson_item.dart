caa32947d7fc6e7f4e38fb39298cdf732b90c960
gg1 <gerardmarcosfreixas@gmail.com>
2021-06-14 00:35:00 +0200

    Grouped tests to calculate the average values.

--- src/ai_mobile_app/lib/screens/home/lesson_item.dart
+++ src/ai_mobile_app/lib/screens/home/lesson_item.dart
@@ -3,30 +3,6 @@ import 'package:ai_mobile_app/screens/home/lesson_page.dart';
 import 'package:flutter/material.dart';
 import 'package:ai_mobile_app/screens/home/lessons.dart';
 
-/*class LessonItem extends StatelessWidget {
-  final CustomTest test;
-
-  LessonItem({ required this.test });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 10.0),
-      child: Card(
-        margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
-        child: ListTile(
-          title: Text(test.name),
-          subtitle: Text('First test (${test.name})'),
-          leading: CircleAvatar(
-            radius: 25.0,
-            backgroundColor: Colors.lightBlue,
-          ),
-        ),
-      ),
-    );
-  }
-}*/
-
 class LessonItem extends StatelessWidget {
   final int? index;
   final IconData? icon;
--- src/ai_mobile_app/lib/screens/home/summary_collective_list.dart
+++ src/ai_mobile_app/lib/screens/home/summary_collective_list.dart
@@ -1,5 +1,6 @@
 import 'package:ai_mobile_app/custom_widgets/summary_collective_test_tile.dart';
 import 'package:ai_mobile_app/custom_widgets/summary_individual_test_tile.dart';
+import 'package:ai_mobile_app/data/test_questions.dart';
 import 'package:ai_mobile_app/models/custom_test.dart';
 import 'package:ai_mobile_app/services/database.dart';
 import 'package:flutter/material.dart';
@@ -15,13 +16,48 @@ class _SummaryCollectiveListState extends State<SummaryCollectiveList> {
   @override
   Widget build(BuildContext context) {
     final tests = Provider.of<List<CustomTest>?>(context) ?? [];
-    print(tests);
+    List<List<CustomTest?>> groupedTests = _groupTestsByTestId(tests);
 
     return ListView.builder(
-      itemCount: tests.length,
+      itemCount: questionLibrary.length,
       itemBuilder: (context, index) {
         return SummaryCollectiveTestTile(test: tests[index]);
       },
     );
+  }
+
+  List<List<CustomTest?>> _groupTestsByTestId(List<CustomTest>? tests) {
+    List<List<CustomTest?>> _groupedTests = [[]];
+
+    for (CustomTest t in tests!) {
+      switch (t.testId!) {
+        case 1:
+          {
+            _groupedTests[0].add(t);
+          }
+          break;
+        case 2:
+          {
+            _groupedTests[1].add(t);
+          }
+          break;
+        case 3:
+          {
+            _groupedTests[2].add(t);
+          }
+          break;
+        case 4:
+          {
+            _groupedTests[3].add(t);
+          }
+          break;
+
+        default:
+          {}
+          break;
+      }
+    }
+
+    return _groupedTests;
   }
 }
