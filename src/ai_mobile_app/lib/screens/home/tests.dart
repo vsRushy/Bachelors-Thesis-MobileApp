54ecfab650f7cfe652e794f7a1f0d2c463cf47b0
gg1 <gerardmarcosfreixas@gmail.com>
2021-06-15 20:59:04 +0200

    Correctly adding experience and points depending on the test.

--- src/ai_mobile_app/lib/screens/home/tests.dart
+++ src/ai_mobile_app/lib/screens/home/tests.dart
@@ -7,6 +7,7 @@ import 'package:ai_mobile_app/screens/home/test_page.dart';
 import 'package:ai_mobile_app/models/test_item.dart';
 import 'package:ai_mobile_app/services/auth.dart';
 import 'package:ai_mobile_app/services/database.dart';
+import 'package:ai_mobile_app/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
@@ -42,65 +43,79 @@ class _TestsState extends State<Tests> {
   Widget build(BuildContext context) {
     final user = Provider.of<CustomUser?>(context);
 
-    return ListView.builder(
-      padding: EdgeInsets.all(25),
-      itemCount: tests.length,
-      itemBuilder: (context, index) {
-        return Container(
-          padding: EdgeInsets.only(
-            bottom: 20,
-          ),
-          margin: EdgeInsets.only(bottom: 25),
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                blurRadius: 10,
-                spreadRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment(1.2, 0.0),
-              colors: <Color>[Colors.white, Colors.lightBlue],
-              tileMode: TileMode.repeated,
-            ),
-          ),
-          child: ListTile(
-            leading: Icon(Icons.book),
-            title: Text(tests[index].name!),
-            subtitle: Text('Points: 10  Experience: 100'),
-            //trailing: Icon(Icons.expand_more), TODO on summary?
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => TestPage(
-                    category: QuestionCategory(
-                      name: "Test $index",
-                      questions: questionLibrary[index],
+    return StreamBuilder<UserData?>(
+      stream: DatabaseService(uid: user!.uid).userData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          UserData userData = snapshot.data!;
+
+          return ListView.builder(
+            padding: EdgeInsets.all(25),
+            itemCount: tests.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.only(
+                  bottom: 20,
+                ),
+                margin: EdgeInsets.only(bottom: 25),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                      offset: Offset(0, 3),
                     ),
-                    testIndex: index,
+                  ],
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment(1.2, 0.0),
+                    colors: <Color>[Colors.white, Colors.lightBlue],
+                    tileMode: TileMode.repeated,
                   ),
                 ),
-              ).then((receivedTest) {
-                CustomTest t = receivedTest;
-                DatabaseService(uid: user!.uid).createCustomTest(
-                  t.testId!,
-                  t.mark!,
-                  t.correctAnswers!,
-                  t.incorrectAnswers!,
-                );
-              });
+                child: ListTile(
+                  leading: Icon(Icons.book),
+                  title: Text(tests[index].name!),
+                  subtitle: Text('Points: 10  Experience: 100'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TestPage(
+                          category: QuestionCategory(
+                            name: "Test $index",
+                            questions: questionLibrary[index],
+                          ),
+                          testIndex: index,
+                        ),
+                      ),
+                    ).then((receivedTest) async {
+                      CustomTest t = receivedTest;
+                      await DatabaseService(uid: user.uid).createCustomTest(
+                        t.testId!,
+                        t.mark!,
+                        t.correctAnswers!,
+                        t.incorrectAnswers!,
+                      );
+                      await DatabaseService(uid: user.uid).updateUserData(
+                        userData.experience += 50,
+                        userData.points += (t.mark! ~/ 10).toInt(),
+                      );
+                    });
+                  },
+                  enabled: true, // TODO: change for locked tests
+                ),
+              );
             },
-            enabled: true, // TODO: change for locked tests
-          ),
-        );
+          );
+        } else {
+          return Loading();
+        }
       },
     );
   }
