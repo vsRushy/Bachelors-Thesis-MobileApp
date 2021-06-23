64d1dc3877542b7f96d1ab5ab8b6815e82dbfb06
gg1 <gerardmarcosfreixas@gmail.com>
2021-06-23 12:24:30 +0200

    Added lesson 5 card.

--- src/ai_mobile_app/lib/screens/home/lessons.dart
+++ src/ai_mobile_app/lib/screens/home/lessons.dart
@@ -20,7 +20,8 @@ class _LessonsState extends State<Lessons> {
           icon: Icons.looks_one,
           title: 'Lesson 1',
           subTitle: 'AI & ML concepts and Environment setup',
-          description: 'Learn the main concepts of Artificial Intelligence and Machine Learning at the same time of setting up the Environment.',
+          description:
+              'Learn the main concepts of Artificial Intelligence and Machine Learning at the same time of setting up the Environment.',
           moreInfo: "This lesson doesn't have a test.",
         ),
         SizedBox(
@@ -31,7 +32,8 @@ class _LessonsState extends State<Lessons> {
           icon: Icons.looks_two,
           title: 'Lesson 2',
           subTitle: 'Phase 1: Target finder',
-          description: 'Development of an agent that finds a defined target in any direction.',
+          description:
+              'Development of an agent that finds a defined target in any direction.',
         ),
         SizedBox(
           height: 25.0,
@@ -41,7 +43,8 @@ class _LessonsState extends State<Lessons> {
           icon: Icons.looks_3,
           title: 'Lesson 3',
           subTitle: 'Phase 2: Autonomous car using checkpoints.',
-          description: 'Create a car that drives autonomously using the checkpoints training method.',
+          description:
+              'Create a car that drives autonomously using the checkpoints training method.',
         ),
         SizedBox(
           height: 25.0,
@@ -51,10 +54,18 @@ class _LessonsState extends State<Lessons> {
           icon: Icons.looks_4,
           title: 'Lesson 4',
           subTitle: 'Phase 3: Autonomous car without checkpoints.',
-          description: 'Develop a car that drives autonomously without the necessity of using checkpoints.',
+          description:
+              'Develop a car that drives autonomously without the necessity of using checkpoints.',
         ),
         SizedBox(
           height: 25.0,
+        ),
+        LessonItem(
+          index: 5,
+          icon: Icons.looks_5,
+          title: 'Lesson 5',
+          subTitle: 'Artificial Neural Networks',
+          description: 'Deep into the concept of an ANN.',
         ),
       ],
     );
